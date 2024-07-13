31acd4ffc42c5f0cd2b41002da87cd69bef7e08e
Amina Sultan <aminasultan017@gmail.com>
2024-07-13 17:41:55 +0500

    Incorporated minor updates

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -4,7 +4,6 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   useEffect(() => {
     const token = localStorage.getItem('token');
     setIsLoggedIn(!!token);
